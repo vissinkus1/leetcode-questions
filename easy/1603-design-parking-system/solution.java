c50f700fbaf70f8d8d67a14b2311cb28b214ca63
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-08-22 16:00:08 +0530

    ✅ Design Parking System | Easy | Java | Runtime: 8 ms (60.41%) | Memory: 47 MB | 2026-08-22

---
+++ easy/1603-design-parking-system/solution.java
@@ -0,0 +1,51 @@
+class ParkingSystem {
+
+    int big;
+    int medium;
+    int small;
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+    
+    public boolean addCar(int carType) {
+        
+        if(carType == 1) {
+            if(big > 0){
+                big--;
+                return true;
+
+            } 
+            else {
+                return false;
+            }
+
+        }
+
+        if(carType == 2) {
+            if(medium > 0){
+                medium--;
+                return true;
+
+            } 
+            else {
+                return false;
+            }
+
+        }
+
+        if(carType == 3) {
+            if(small > 0){
+                small--;
+                return true;
+
+            } 
+            else {
+                return false;
+            }
+
+        }
+        return false;
+    }
+}
