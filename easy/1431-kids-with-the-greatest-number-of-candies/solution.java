87eaf981ecd35388a7617bdcced5c9feb2f77334
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-08-22 16:02:18 +0530

    ✅ Kids With the Greatest Number of Candies | Easy | Java | Runtime: 1 ms (96.39%) | Memory: 43.9 MB | 2026-08-22

---
+++ easy/1431-kids-with-the-greatest-number-of-candies/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max=0;
+        for(int candy:candies){
+             max=Math.max(max,candy);
+        }
+        List<Boolean>result=new ArrayList<>();
+        for(int candy:candies){
+            if(candy + extraCandies>=max){
+                result.add(true);
+            }
+            else{
+             result.add(false);
+            }
+        }
+        return result;
+            
+        
+        
+    }
+}
