8c34ca959784eb0b05ae07ba1fcd66dba43620c1
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-08-23 18:16:09 +0530

    ✅ Reverse String | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-23

--- easy/0344-reverse-string/solution.java
+++ easy/0344-reverse-string/solution.java
@@ -1,12 +1,13 @@
-class Solution {
-    public void reverseString(char[] s) {
-        int left = 0, right = s.length - 1;
-        while (left < right) {
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
-        }
-    }
+        int i = 0, j = s.length - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
 }
+
+    public void reverseString(char[] s) {
+class Solution {
