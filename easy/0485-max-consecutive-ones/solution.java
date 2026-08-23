963af46f027caf2702bd9220fe6f5da940b6aacc
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-08-23 14:59:12 +0530

    ✅ Max Consecutive Ones | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-23

--- easy/0485-max-consecutive-ones/solution.java
+++ easy/0485-max-consecutive-ones/solution.java
@@ -1,15 +1,16 @@
-            }
         }
-           // else if(nums[i]==0){
+            else if(nums[i]==0){
 
             }
 
-             //   break;
-            //}
+               break;
+            }
         
-                count++;
-            if(i==i+1){
-        for(int i=0;i<nums.length;i++){
-    }
              return count;
-
+            }
+                count++;
+            if(nums[i]==nums[i]+1){
+        for(int i=0;i<nums.length;i++){
+        int count=0;
+    public int findMaxConsecutiveOnes(int[] nums) {
+class Solution {
