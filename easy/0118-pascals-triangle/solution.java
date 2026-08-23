34588d88f5f249b6c7b019c1e828bd0bb3bf1ac4
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-08-23 18:04:55 +0530

    ✅ Pascal's Triangle | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-23

---
+++ easy/0118-pascals-triangle/solution.java
@@ -0,0 +1,15 @@
+        newRow.add(1);
+        for(int i=1;i<row;i++){
+        List<Integer>prevRow = result.get(row-1);
+            newRow.add(prevRow.get(i)+prevRow.get(i-1));
+        List<Integer> newRow=new ArrayList<>();
+      for(int row=1;row<numRows;row++){
+     //
+     //1
+     result.get(0).add(1);
+     result.add(new ArrayList<Integer>());
+    //numrow=5
+
+        List<List<Integer>> result=new ArrayList<List<Integer>>();
+    public List<List<Integer>> generate(int numRows) {
+class Solution {
