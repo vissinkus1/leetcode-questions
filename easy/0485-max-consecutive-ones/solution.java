08d540a91fd9182536774e93e326859040f916cc
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-08-23 14:56:59 +0530

    ✅ Max Consecutive Ones | Easy | Java | Runtime: 0 ms | Memory: 43 MB | 2026-08-23

---
+++ easy/0485-max-consecutive-ones/solution.java
@@ -0,0 +1,15 @@
+            }
+        }
+            else if(nums[i]==0){
+
+            }
+
+                break;
+            }
+        
+                count++;
+            if(i==i+1){
+        for(int i=0;i<nums.length;i++){
+    }
+             return count;
+
