72cdd76b184d2a7894fec36eb5c893102c2b9a11
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-09-01 13:51:11 +0530

    ✅ Keys and Rooms | Medium | Java | Runtime: 1 ms (83.87%) | Memory: 46.4 MB | 2026-09-01

---
+++ medium/0841-keys-and-rooms/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(0, rooms, visited);
+
+        for (boolean v : visited) {
+            if (!v) return false;
+        }
+        return true;
+    }
+
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
+        if (visited[room]) return;
+        visited[room] = true;
+
+        for (int key : rooms.get(room)) {
+            dfs(key, rooms, visited);
+        }
+    }
+}
