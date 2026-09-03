82cd576259ab7f7c04a7423010f3ce5e1ea9f4d9
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-09-03 12:01:09 +0530

    ✅ Longest Valid Parentheses | Hard | Java | Runtime: 2 ms (96.86%) | Memory: 45.3 MB | 2026-09-03

---
+++ hard/0032-longest-valid-parentheses/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        int[] dp = new int[n];
+        int maxLen = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] - 1 >= 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + 2 + ((i - dp[i - 1] - 2 >= 0) ? dp[i - dp[i - 1] - 2] : 0);
+                }
+                maxLen = Math.max(maxLen, dp[i]);
+            }
+        }
+        return maxLen;
+    }
+}
