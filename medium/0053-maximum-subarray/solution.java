4b5a44279759c2bfe79f54567472a1d3d4f7b933
Vishal singh kushwaha <singhkushwahavishal344@gmail.com>
2026-09-16 00:11:30 +0530

    ✅ Maximum Subarray | Medium | Java | Runtime: 1 ms (99.92%) | Memory: 77.1 MB | 2026-09-15

---
+++ medium/0053-maximum-subarray/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+       // Maximum sum
+        long maxi = Long.MIN_VALUE; 
+        
+        // Current sum of subarray 
+        long sum = 0; 
+        
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            
+            // Add current element to the sum
+            sum += nums[i]; 
+            
+            // Update maxi if current sum is greater
+            if (sum > maxi) {
+                maxi = sum; 
+            }
+            
+            // Reset sum to 0 if it becomes negative
+            if (sum < 0) {
+                sum = 0; 
+            }
+        }
+        
+        // Return the maximum subarray sum found
+        return (int) maxi;
+    }
+}
